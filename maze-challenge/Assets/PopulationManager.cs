ccd109da2d35c5a3ad5d64d5c9b2da0462df5ff0
TurtleXD101 <103543218+TurtleXD101@users.noreply.github.com>
2024-03-22 17:46:13 +1100

    fixed stuff i think maybe ?

--- maze-challenge/Assets/PopulationManager.cs
+++ maze-challenge/Assets/PopulationManager.cs
@@ -25,7 +25,7 @@ namespace MazeWalking {
             GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
             GUI.Label(new Rect(10, 25, 200, 30), "Gen: " + generation, guiStyle);
             GUI.Label(new Rect(10, 50, 200, 30), string.Format("Time: {0:0.00}", elapsed), guiStyle);
-            GUI.Label(new Rect(10, 75, 200, 30), "Population" + population.Count, guiStyle);
+            GUI.Label(new Rect(10, 75, 200, 30), "Population: " + population.Count, guiStyle);
             GUI.EndGroup();
         }
 
